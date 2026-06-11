5717b46db1191f0c8948968cfe1806b3e5df1f6b
Artem Nersisyan <ar5tem5@mail.ru>
2026-06-11 20:14:57 +0300

    Текст не вылазит за рамки

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rz-reader-md-v10-copy';
+const CACHE='rz-reader-md-v11-textfit';
 const CORE=['./','index.html','assets/styles.css','assets/app.js','content/arcs.json','content/arc-6/arc.json','content/arc-6/volumes.json','content/arc-6/volume-21/chapters.json','content/arc-6/volume-22/chapters.json','content/arc-6/volume-23/chapters.json','content/arc-6/volume-24/chapters.json','content/arc-6/volume-25/chapters.json','content/arc-6/gallery.json','content/ln-extras/arc.json','content/ln-extras/gallery.json','content/ln-extras/volumes.json','manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
